f99751ddc6250afc55c3a88f1e5fd17e0a73e193
rumyantsevaaa <32551056+rumyantsevaaa@users.noreply.github.com>
2018-03-16 21:40:23 +0300

    Add files via upload

---
+++ r3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	int s1, s2, diff;
+	s1 = 15;
+	s2 = 87;
+	diff = s2 - s1;
+	printf("Результатом вычитания %i из %i является %i", s1, s2, diff);
+	return 0;
+}
